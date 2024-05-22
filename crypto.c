38ceed2189f0780b55019afc8d389fb0e5e07c73
Connor Grayden <87635341+Connor2803@users.noreply.github.com>
2024-05-22 23:42:25 +0800

    Make more secure
    Change from strcmp to strncmp. Also callocs for the size of result now to get avoid a static analysis error. Still getting an error for strlen but the preconditions state that strings are null terminated so should be fine.

--- crypto.c
+++ crypto.c
@@ -1,3 +1,5 @@
+// Connor Grayden (23349066)
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,13 +7,15 @@
 #include <ctype.h>
 #include "crypto.h"
 
+#define BUFFER_SIZE 1024
+
 /**
  * Encrypt using caesar
  */
 void caesar_encrypt(char range_low, char range_high, int key, const char *plain_text, char *cipher_text) {
     size_t range = (size_t)(range_high - range_low + 1);
 
-    for (size_t i = 0; plain_text[i] != '\0'; ++i) {
+    for (size_t i = 0; plain_text[i] != '\0'; i++) {
         if (plain_text[i] >= range_low && plain_text[i] <= range_high) {
             cipher_text[i] = (char)(range_low + (int)((plain_text[i] - range_low + key + (int)range) % (int)range));
         } else {
@@ -34,7 +38,7 @@ void caesar_decrypt(char range_low, char range_high, int key, const char *cipher
 void vigenere_encrypt(char range_low, char range_high, const char *key, const char *plain_text, char *cipher_text) {
     size_t key_len = strlen(key);
     size_t range = (size_t)(range_high - range_low + 1);
-    for (size_t i = 0, j = 0; plain_text[i] != '\0'; ++i) {
+    for (size_t i = 0, j = 0; plain_text[i] != '\0'; i++) {
         if (plain_text[i] >= range_low && plain_text[i] <= range_high) {
             cipher_text[i] = (char)(range_low + (int)((plain_text[i] - range_low + key[j % key_len] - range_low + (int)range) % (int)range));
             j++;
@@ -51,7 +55,7 @@ void vigenere_encrypt(char range_low, char range_high, const char *key, const ch
 void vigenere_decrypt(char range_low, char range_high, const char *key, const char *cipher_text, char *plain_text) {
     size_t key_len = strlen(key);
     size_t range = (size_t)(range_high - range_low + 1);
-    for (size_t i = 0, j = 0; cipher_text[i] != '\0'; ++i) {
+    for (size_t i = 0, j = 0; cipher_text[i] != '\0'; i++) {
         if (cipher_text[i] >= range_low && cipher_text[i] <= range_high) {
             plain_text[i] = (char)(range_low + (int)((cipher_text[i] - range_low - (key[j % key_len] - range_low) + (int)range) % (int)range));
             j++;
@@ -75,66 +79,72 @@ int cli(int argc, char **argv) {
     const char *operation = argv[1];
     const char *key_str = argv[2];
     const char *message = argv[3];
-    char result[strlen(message) + 1];
+
+    size_t message_size = strlen(message);
+    char *result = calloc(message_size + 1, sizeof(char));
 
     // Check if the key is empty
-    if (strcmp(key_str, "") == 0) {
+    if (strncmp(key_str, "", BUFFER_SIZE) == 0) {
         fprintf(stderr, "Error: No key provided.\n");
+        free(result);
         return 1;
     }
-
-    if (strcmp(operation, "caesar-encrypt") == 0 || strcmp(operation, "caesar-decrypt") == 0) {
+    // could change these buffers to be the exact size of the string
+    if (strncmp(operation, "caesar-encrypt", BUFFER_SIZE) == 0 || strncmp(operation, "caesar-decrypt", BUFFER_SIZE) == 0) {
         char *endptr;
-
-        
 
         long key = strtol(key_str, &endptr, 10);
 
         // Check if the conversion was successful
         if (*endptr != '\0' || endptr == key_str) {
             fprintf(stderr, "Error: Key must be a valid integer.\n");
+            free(result);
             return 1;
         }
 
         // Check if the key is within the range of INT_MIN and INT_MAX
         if (key < INT_MIN || key > INT_MAX) {
             fprintf(stderr, "Error: Key out of range.\n");
+            free(result);
             return 1;
         }
 
         // Normalize the key to the range of -25 to 25 for the Caesar cipher
         // Just making it into its positive equivalent if negative though
-        int normalized_key = (int)key % 26;
-        if (normalized_key < 0) {
-            normalized_key += 26;
+        key = (int)key % 26;
+        if (key < 0) {
+            key += 26;
         }
-
+        // these too!
         // Run either encrypt or decrypt
-        if (strcmp(operation, "caesar-encrypt") == 0) {
-            caesar_encrypt('A', 'Z', normalized_key, message, result);
+        if (strncmp(operation, "caesar-encrypt", BUFFER_SIZE) == 0) {
+            caesar_encrypt('A', 'Z', key, message, result);
         } else {
-            caesar_decrypt('A', 'Z', normalized_key, message, result);
+            caesar_decrypt('A', 'Z', key, message, result);
         }
-    } else if (strcmp(operation, "vigenere-encrypt") == 0 || strcmp(operation, "vigenere-decrypt") == 0) {
+    } else if (strncmp(operation, "vigenere-encrypt", BUFFER_SIZE) == 0 || strncmp(operation, "vigenere-decrypt", BUFFER_SIZE) == 0) {
 
-        for (size_t i = 0; key_str[i] != '\0'; ++i) {
+        for (size_t i = 0; key_str[i] != '\0'; i++) {
             if (key_str[i] < 'A' || key_str[i] > 'Z') {
                 fprintf(stderr, "Error: Key contains characters out of range. Must be within 'A' to 'Z'.\n");
+                free(result);
                 return 1;
             }
         }
 
-        if (strcmp(operation, "vigenere-encrypt") == 0) {
+        if (strncmp(operation, "vigenere-encrypt", BUFFER_SIZE) == 0) {
             vigenere_encrypt('A', 'Z', key_str, message, result);
         } else {
             vigenere_decrypt('A', 'Z', key_str, message, result);
         }
     } else {
         fprintf(stderr, "Error: Invalid operation. Must be ['caesar-encrypt' | 'caesar-decrypt' | 'vigenere-encrypt' | 'vigenere-decrypt'].\n");
+        free(result);
         return 1;
     }
 
     printf("%s\n", result);
+    free(result);
     return 0;
 }
 
